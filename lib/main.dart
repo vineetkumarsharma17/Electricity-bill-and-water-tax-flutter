05b46f29c4975d5fa52cd5760ce66243ef7b955d
VINEET KUMAR SHARMA <vineetkumarsharma17@gmail.com>
2021-08-05 03:16:53 +0530

    this is first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bill_calculator/electricity.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyElectricityBill(),
+    );
+  }
+}
+
+
